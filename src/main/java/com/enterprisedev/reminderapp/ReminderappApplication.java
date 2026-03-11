8eeae8b35f1f6b20ca31aaf0bbf85ceee4155519
Jon Sanjuan <jonathan.s.sanjuan@outlook.com>
2026-03-10 20:43:17 -0400

    Add Spring Initializr to start Spring Boot Project

---
+++ src/main/java/com/enterprisedev/reminderapp/ReminderappApplication.java
@@ -0,0 +1,13 @@
+package com.enterprisedev.reminderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderappApplication.class, args);
+	}
+
+}
